12f92d6d35c600580f164b7e5e239685ae43ea49
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2020-03-30 22:36:09 +0300

    Fix checkbox component
    Change css and smoll fixing
    Add ESLint and Prettier config and setting from https://maxpfrontend.ru/vebinary/nastroyka-eslint-prettier-pre-commit-hook-create-react-app-visual-studio-code/

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React from 'react';
-import {Button} from "./components/button/Button";
-import {Checkbox} from "./components/checkbox/Checkbox";
+import { Button } from './components/button/Button';
+import { Checkbox } from './components/checkbox/Checkbox';
 import './App.css';
 
 function App() {
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <Button className="btn-base" size="btn-main" disabled={false}/>
+        <Button className="btn-base" size="btn-main" disabled={false} />
         <br></br>
-        <Button className="btn-base" size="btn-main" disabled={true} color="">DAISABLE</Button>
+        <Button className="btn-base" size="btn-main" disabled={true} color="">
+          DAISABLE
+        </Button>
         <br></br>
-        <Button className="btn-base" size="btn-secondary" disabled={false} >SECOND</Button>
+        <Button className="btn-base" size="btn-secondary" disabled={false}>
+          SECOND
+        </Button>
         <br></br>
-        <Button className="btn-base" size="btn-secondary" disabled={true} >btn-secondary</Button>
+        <Button className="btn-base" size="btn-secondary" disabled={true}>
+          btn-secondary
+        </Button>
         <br></br>
-        <Button className="btn-base" size="btn-tertiary" disabled={false} color="">Start new project</Button>
+        <Button
+          className="btn-base"
+          size="btn-tertiary"
+          disabled={false}
+          color=""
+        >
+          Start new project
+        </Button>
         <br></br>
-        <Button className="btn-base" size="btn-tertiary" disabled={true}>btn-tertiary</Button>
+        <Button className="btn-base" size="btn-tertiary" disabled={true}>
+          btn-tertiary
+        </Button>
         <br></br>
-        {/* <Checkbox className="chek-base" size="chek-main" disabled={false} color="chek-main" id="checkbox"  onChange={() => {}} type="checkbox"></Checkbox>
-        <Checkbox className="chek-base" size="chek-main" disabled={true} color="chek-main" id="checkbox" name="checkbox"   onChange={() => {}} type="checkbox"></Checkbox>
-        <Checkbox className="chek-base" size="chek-main" disabled={false} checked color="chek-main" id="checkbox"  onChange={() => {}} type="checkbox"></Checkbox>
-        <Checkbox className="chek-base" size="chek-main" disabled={true} checked color="chek-main" id="checkbox" name="checkbox"  onChange={() => {}} type="checkbox"></Checkbox> */}
-        <Checkbox className="chek-base" size="chek-main" id="cb1" />
-        <Checkbox className="chek-base" size="chek-secondary"  id="cb2" />
-        <Checkbox className="chek-base" color="chek-success" id="cb3" />
-        <Checkbox className="chek-base" checked id="cb4" />
+        <Checkbox
+          className="chek-base"
+          disabled={false}
+          color="chek-main"
+          label="test label"
+          id="cb1"
+        />
+        <Checkbox
+          className="chek-base"
+          disabled={true}
+          color="chek-main"
+          id="cb2"
+        />
+        <Checkbox
+          className="chek-base"
+          disabled={false}
+          color="chek-success"
+          id="cb3"
+        />
       </header>
     </div>
   );
-  
 }
-
 
 export default App;
--- src/components/checkbox/Checkbox.jsx
+++ src/components/checkbox/Checkbox.jsx
@@ -1,71 +1,79 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Checkbox.css';
 
+class CheckboxComponent extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onChange = this.onChange.bind(this);
+  }
 
-
-class CheckboxComponent extends React.PureComponent{
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-       
-      }
-
-    render() {
-        let {
-            id,
-            type,
-            label, 
-            error,
-            children,
-            active,
-            className,   
-            disabled,    
-            color,  
-            size,
-            onClick,
-            ...attributes
-          } = this.props;
-
-        let checkClass = `${className} ${size} ${color}`; 
-
-        // const Tag = attributes.checkbox ? 'a' : 'Checkbox';
-
-        // console.log("mij class", checkClass);
-
-        return (
-          <div className ="checkbox">
-                <input
-                  type={type}
-                  name={id}
-                  id={id}
-                  className={checkClass}
-                  size={size}
-                  {...attributes}
-                />
-                <label for={id}>чекбокс {id}</label>
-          </div>     
-        );
+  onChange(e) {
+    if (this.props.disabled) {
+      e.preventDefault();
+      return;
     }
 
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
+  }
+
+  render() {
+    const {
+      id,
+      type,
+      label,
+      error,
+      children,
+      active,
+      className,
+      disabled,
+      color,
+      size,
+      onClick,
+      checked,
+      ...attributes
+    } = this.props;
+
+    let checkClass = `${className} ${size} ${color}`;
+
+    return (
+      <div className="checkbox">
+        <input
+          type={type}
+          name={id}
+          id={id}
+          className={checkClass}
+          size={size}
+          disabled={disabled}
+          {...attributes}
+        />
+        <label htmlFor={id}>{this.props.label}</label>
+      </div>
+    );
+  }
 }
 
 CheckboxComponent.propTypes = {
-    'aria-label': PropTypes.string,
-    color: PropTypes.string, // default: 'secondary'
-    disabled: PropTypes.bool,
-    onClick: PropTypes.func,
-    size: PropTypes.string, // main / secondary / tertiary
-    className: PropTypes.string,
+  'aria-label': PropTypes.string,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  size: PropTypes.string, // main / secondary / tertiary
+  className: PropTypes.string,
+  checked: PropTypes.bool,
+  label: PropTypes.string,
 };
 
 CheckboxComponent.defaultProps = {
-    disabled: false,
-    className: 'chek-base',
-    type:'checkbox'
-    };
+  disabled: false,
+  className: 'chek-base',
+  type: 'checkbox',
+  checked: false,
+  label: '',
+};
 
 export const Checkbox = CheckboxComponent;
